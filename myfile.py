3fc3034e6cac6aaef0c74cdc1dacf3a091381007
MaximoGuzmanH <maximoguzmanhuaman@gmail.com>
2024-11-04 21:52:32 -0500

    Update myfile.py
    Se agregaron 2 nuevos gráficos de Pie

--- myfile.py
+++ myfile.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 @st.cache_resource
 def load_and_process_data(file_path):
@@ -76,13 +77,11 @@ st.map(df_filtered[['Latitud', 'Longitud']].rename(columns={'Latitud': 'latitude
 # Mostrar la tabla de datos en Streamlit excluyendo ciertas columnas
 st.dataframe(df_filtered.drop(columns=['ID Centro de Vacunacion', 'Latitud', 'Longitud', 'id_eess']))
 
-# Gráfico de pastel para la distribución de centros por entidad administradora
+# Gráfico de pastel para la distribución de centros por entidad administradora (Top 4 + Otros)
 st.subheader("Distribución de Centros de Vacunación por Entidad Administradora (Top 4 + Otros)")
 
 # Calcular la cantidad de centros por entidad administradora
 entidad_counts = df_filtered['Entidad Administradora'].value_counts()
-
-# Seleccionar los top 5 y agrupar el resto como "Otros"
 top_4_entidades = entidad_counts.nlargest(4)
 otros = entidad_counts[4:].sum()
 entidad_labels = list(top_4_entidades.index) + ['Otros']
@@ -92,10 +91,55 @@ entidad_sizes = list(top_4_entidades.values) + [otros]
 fig, ax = plt.subplots()
 ax.pie(entidad_sizes, labels=entidad_labels, autopct='%1.1f%%', startangle=90,
        pctdistance=0.85, labeldistance=1.1, colors=plt.cm.Paired.colors)
-
-# Asegura que el gráfico sea circular y mejora la legibilidad
-ax.axis('equal')
+ax.axis('equal')  # Asegura que el gráfico sea circular
 plt.tight_layout()
-
-# Mostrar el gráfico en Streamlit
 st.pyplot(fig)
+
+# Gráfico de pastel para la distribución de centros por departamento (Top 5 + Otros)
+st.subheader("Distribución de Centros de Vacunación por Departamento (Top 5 + Otros)")
+
+centros_vacunacion_porDept = df_filtered[['Departamento', 'ID Centro de Vacunacion']].copy()
+centros_vacunacion_porDept['cant_centros'] = 1
+centros_vacunacion_porDept = centros_vacunacion_porDept.groupby('Departamento', as_index=False).sum()
+centros_vacunacion_porDept = centros_vacunacion_porDept.sort_values('cant_centros', ascending=False).reset_index(drop=True)
+
+top5_Dept_porCantCentros = centros_vacunacion_porDept[:5]
+otros_Dept_porCantCentros = pd.DataFrame(
+    [['OTROS', centros_vacunacion_porDept['cant_centros'][5:].sum()]],
+    columns=['Departamento', 'cant_centros']
+)
+centros_vacunacion_porDept_top5_y_otros = pd.concat([top5_Dept_porCantCentros, otros_Dept_porCantCentros])
+
+piec_centros_porDept = px.pie(
+    centros_vacunacion_porDept_top5_y_otros,
+    values='cant_centros', names='Departamento',
+    title='Centros por Departamento',
+    category_orders={"Departamento": ["OTROS"] + list(top5_Dept_porCantCentros['Departamento'][::-1])}
+)
+st.plotly_chart(piec_centros_porDept)
+
+# Selección de Departamento y gráfico de centros por provincia
+st.subheader("Distribución de Centros de Vacunación por Provincia en Departamento Seleccionado")
+
+departamentos_opciones = df_filtered['Departamento'].unique()
+departamento_seleccionado = st.selectbox('Elija el departamento del cual desee mayor detalle', departamentos_opciones)
+
+centros_por_provincia = df_filtered[df_filtered['Departamento'] == departamento_seleccionado][['Provincia', 'ID Centro de Vacunacion']].copy()
+centros_por_provincia['cant_centros'] = 1
+centros_por_provincia = centros_por_provincia.groupby('Provincia', as_index=False).sum()
+centros_por_provincia = centros_por_provincia.sort_values('cant_centros', ascending=False).reset_index(drop=True)
+
+top5_Prov_porCantCentros = centros_por_provincia[:5]
+otras_Prov_porCantCentros = pd.DataFrame(
+    [['OTRAS', centros_por_provincia['cant_centros'][5:].sum()]],
+    columns=['Provincia', 'cant_centros']
+)
+centros_vacunacion_porProv_top5_y_otras = pd.concat([top5_Prov_porCantCentros, otras_Prov_porCantCentros])
+
+piec_centros_porProv = px.pie(
+    centros_vacunacion_porProv_top5_y_otras,
+    values='cant_centros', names='Provincia',
+    title='Centros por Provincia',
+    category_orders={"Provincia": ["OTRAS"] + list(top5_Prov_porCantCentros['Provincia'][::-1])}
+)
+st.plotly_chart(piec_centros_porProv)
